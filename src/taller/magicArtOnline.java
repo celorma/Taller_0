f2f767a31b0ce5574161b91000abf5143f098d52
celorma <89708923+celorma@users.noreply.github.com>
2022-04-17 16:21:52 -0400

    inicio de sesión integrado

--- src/taller/magicArtOnline.java
+++ src/taller/magicArtOnline.java
@@ -4,10 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class magicArtOnline {
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner sc = new Scanner (System.in);
-		
+public class magicArtOnline 
+{
+	public static void main(String[] args) throws FileNotFoundException 
+	{
 		File filePlayers = new File ("Jugadores.txt");
 		File fileSpells = new File ("Hechizos.txt");
 		File filePlayerSpells = new File ("HechizosJugadores.txt");
@@ -19,6 +19,11 @@ public class magicArtOnline {
 		Scanner archEnemy = new Scanner(fileEnemy);
 		
 		int longArray = 100;
+		
+		String user = null, password = null;
+		String confirmation = null;
+		
+		String[] newPlayer = new String[] {"nuevoUsuario","nuevaContraseña","5","5","5","5","1","0"};
 		
 		String[][] enemyData = new String[][] {{"F","100" ,"75","1" },
 							                   {"C","250" ,"50","2" },
@@ -35,9 +40,72 @@ public class magicArtOnline {
 		fillArray(archSpells, spells);
 		fillArray(archPlayerSpells, playerSpells);
 		fillArray(archEnemy, enemies);
-    printArray(players,0);
 		
-		sc.close();
+		do
+		{
+			user = input("Ingrese su nombre de usuario: ");
+			password = input("Ingrese su contraseña: ");
+			division();
+			
+			if (!search(players,user,0,0)) 
+			{
+		    	confirmation = input("Usuario no encontrado, desea registrarse?(SI/NO): ");
+		    	
+		    	if (confirmation.equals("SI"))
+				{
+		    		do
+		    		{
+						user = input("Ingrese un nuevo nombre de usuario: ");
+						password = input("Ingrese una nueva contraseña: ");
+						
+						if (!search(players,user,0,0)) 
+						{
+							System.out.println();
+							System.out.println("Registrado correctamente!");
+							newPlayer[0] = user;
+							newPlayer[1] = password;
+							enterListIntoArray(newPlayer, players);
+							division();
+							break;
+						}
+						
+						else
+						{
+							division();
+							System.out.println("Nombre de usuario no disponible, ingrese uno distinto");
+						}
+						
+		    		}while (search(players,user,0,0));
+				}
+			}
+			
+			if (!compare(players,user,password))
+			{
+				System.out.println("Contraseña equivocada, intente de nuevo");
+			}
+		
+		}while(!compare(players,user,password));
+ 
+	    
+		
+
+	}
+
+
+
+	private static void division() {
+		System.out.println();
+		System.out.println("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
+		System.out.println();
+	}
+
+
+
+	private static String input(String print) {
+		Scanner sc = new Scanner (System.in);
+		System.out.print(print);
+		String variable = sc.nextLine();
+		return variable;
 	}
 
 	
@@ -76,21 +144,50 @@ public class magicArtOnline {
 		}
 	}
   // en teoria le das una palarabra (chekstring) y este te retorna true o false buscando en la matriz true es que esta y false es que no esta la palabra en la matriz
-	private static void search (String [][] array, boolean kum,String chekstring) 
+	private static boolean search(String [][] array,String chekstring, int initialColumn, int finalColumn) 
 	{
-		for (int i=0;i<array.length;i++) // este for se supone que recorre la matriz entera 
+		for (int i=0;i<array.length;i++)
 		{
-			for (int j=i;j<array[0].length;j++) {
-				if (chekstring == array [i][j] ) {
-					kum = true;
-					System.out.println(kum);
+			for (int j=initialColumn;j<=finalColumn;j++) 
+			{
+				if (array[i][j] != null)
+				{
+					if (array[i][j].equals(chekstring)) 
+					{
+					return true;
 					}
 				}
+			}
+				
 		}
-
+		return false;
 	}
-  
-  {
-    
-  }
+	
+	private static boolean compare(String[][] array,String user1, String password2) // ver si estan en misma fila 
+	{
+		for (int i=0;i<array.length;i++)//cantidad de filas
+		{
+			if (array[i][0] != null && array[i][1] != null)
+			{
+				if (array[i][0].equals(user1) && array[i][1].equals(password2)) 
+		      	{
+		    	  	return true;
+	      		}	
+			}
+		}
+    return false;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
